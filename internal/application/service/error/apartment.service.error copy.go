138db257dd972469e0c3f43b5727d177c98a4f9c
Matin Baki <m9652973@gmail.com>
2026-06-09 20:52:06 +0330

    feat : multi apar access feated ready for test

--- internal/application/service/error/apartment.service.error copy.go
+++ internal/application/service/error/apartment.service.error copy.go
@@ -3,3 +3,4 @@ package error
 import "errors"
 
 var ErrApartmentNotFound = errors.New("apartment not found")
+var ErrApartmentUnauthorizedAccess = errors.New("apartment does not have authorized access")
--- internal/application/service/error/unit.service.error.go
+++ internal/application/service/error/unit.service.error.go
@@ -3,7 +3,8 @@ package error
 import "errors"
 
 var (
-	ErrUnitNotFound      = errors.New("unit_not_found")
-	ErrUnitUpdateFailed  = errors.New("unit_update_failed")
-	ErrUnitAlreadyExists = errors.New("unit_number_already_exists_in_floor")
+	ErrUnitNotFound           = errors.New("unit_not_found")
+	ErrUnitUpdateFailed       = errors.New("unit_update_failed")
+	ErrUnitAlreadyExists      = errors.New("unit_number_already_exists_in_floor")
+	ErrUnitUnauthorizedAccess = errors.New("unit does not have authorized access")
 )
